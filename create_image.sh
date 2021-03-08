1f905a7379b22869a56a5e05c7f5583d365b8c74
Binarysearch <desenlace.net@gmail.com>
2021-03-08 23:17:26 +0100

    v

--- create_image.sh
+++ create_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=0.0.1
+version=0.0.2
 
 docker build --pull --rm -f "Dockerfile" -t genetiushub/artemisa-server-auth:$version "." &&
 docker push genetiushub/artemisa-server-auth:$version
